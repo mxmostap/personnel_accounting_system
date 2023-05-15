427676dfdb1464c03b7024b7fdda98bef3e4a03b
Maxim <maxim.ostapyuk@mail.ru>
2023-05-15 03:35:09 +0300

    Added forms for authorization and registration. Folder for storing profile photos. Styles for pages are enabled.

---
+++ staff/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input form-control', 'required':'true'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input form-control', 'required':'true'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input form-control', 'required':'true'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input form-control', 'required':'true'}))
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class AuthorizationForm(AuthenticationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-control', 'required':'true'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', 'required':'true'}))
